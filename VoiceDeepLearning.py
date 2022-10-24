649249008fd5d9f963ef925035044ac5ecea3c63
Kim Min <kimmin17@sogang.ac.kr>
2022-10-24 14:08:23 +0900

    update

--- VoiceDeepLearning.py
+++ VoiceDeepLearning.py
@@ -1,21 +1,43 @@
 import os
+import torch
 from speechbrain.dataio.dataset import DynamicItemDataset
-import speechbrain
+import speechbrain as sb
 
-@speechbrain.utils.data_pipeline.takes("Path")
-@speechbrain.utils.data_pipeline.provides("Signal")
+class VoiceBrain(sb.Brain):
+    def compute_forward(self, batch, stage):
+        pass
+
+    def compute_objectives(self, batch, stage):
+        pass
+
+@sb.utils.data_pipeline.takes("Path")
+@sb.utils.data_pipeline.provides("Signal")
 def audio_pipeline(path):
-      sig = speechbrain.dataio.dataio.read_audio(path)
+      sig = sb.dataio.dataio.read_audio(path)
       return sig
 
 def make_dataset(json_file):
     dataset = DynamicItemDataset.from_json(json_file)
     dataset.add_dynamic_item(audio_pipeline)
     dataset.set_output_keys(["Signal", "Age", "Gender"])
-    print(dataset[0])
+
+    input_size = 8000
+    tensor_size = dataset[0]["Signal"].size()[0]
+    padding_size = (tensor_size // input_size + 1) * input_size - tensor_size
+
+    padding_tensor = torch.zeros(padding_size)
+    print(padding_tensor.size())
+
+    dataset[0]["Signal"] = torch.cat([dataset[0]["Signal"], padding_tensor], dim = 0)
+
+    print(dataset[0]["Signal"].size())
+
+def model_learning():
+    pass
 
 def voice_learning(json_file):
     make_dataset(json_file)
+    model_learning()
 
 def search_json_file(dirname):
     try:
